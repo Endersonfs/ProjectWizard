115616f36bf362f600409ff70893b2dd7ba7a77c
Paul12pp <30394467+Paul12pp@users.noreply.github.com>
2018-06-21 15:23:52 -0300

    inicio y login

--- ProjectoFinal/ProjectoFinal/MainActivity.cs
+++ ProjectoFinal/ProjectoFinal/MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using System;
+using Android.Content;
 
 namespace ProjectoFinal
 {
@@ -12,8 +14,18 @@ namespace ProjectoFinal
         {
             base.OnCreate(savedInstanceState);
 
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var btn = FindViewById<Button>(Resource.Id.button1);
+            btn.Click += Btn_Click;
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(login));
+            StartActivity(intent);
         }
     }
 }
