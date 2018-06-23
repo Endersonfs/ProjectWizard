a1e195cbe2ab7f9b73cff440f307444f08c52e6b
Raul Sanchez Reyes <chikomalo22@live.com>
2018-06-23 18:14:55 -0400

    Trabajando bottom navigation bar

--- ProjectoFinal/ProjectoFinal/CentroActivity.cs
+++ ProjectoFinal/ProjectoFinal/CentroActivity.cs
@@ -2,36 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Support.Design.Widget;
 
 namespace ProjectoFinal
 {
-    [Activity(Label = "login")]
-    public class login : Activity
+    [Activity(Label = "CentroActivity")]
+    public class CentroActivity : Activity
     {
+        BottomNavigationView bottomNavigation;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            SetContentView(Resource.Layout.main);
             // Create your application here
-            SetContentView(Resource.Layout.login);
-
-
-            var btnLogin = FindViewById<Button>(Resource.Id.button1);
-            btnLogin.Click += BtnLogin_Click;  
-
-        }
-
-        private void BtnLogin_Click(object sender, EventArgs e)
-        {
-            var intent = new Intent(this, typeof(CentroActivity));
-            StartActivity(intent);
         }
     }
 }
