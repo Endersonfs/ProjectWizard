7cb6fa546116b9ea0b0a97607e9397095aa55dc4
Paul12pp <30394467+Paul12pp@users.noreply.github.com>
2018-06-28 15:19:24 -0300

    Api de mapas.

--- ProjectoFinal/ProjectoFinal/Resources/Fragments/FragmentHome.cs
+++ ProjectoFinal/ProjectoFinal/Resources/Fragments/FragmentHome.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
@@ -13,14 +15,30 @@ using Android.Widget;
 
 namespace ProjectoFinal.Resources.Fragments
 {
-    public class FragmentHome : Fragment
+    public class FragmentHome : Fragment, IOnMapReadyCallback
     {
+        private GoogleMap _map;
+        private MapFragment _mapFragment;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
-           
+            _mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+            if (_mapFragment == null)
+            {
+                GoogleMapOptions mapOptions = new GoogleMapOptions()
+                    .InvokeMapType(GoogleMap.MapTypeSatellite)
+                    .InvokeZoomControlsEnabled(false)
+                    .InvokeCompassEnabled(true);
+
+                FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+                _mapFragment = MapFragment.NewInstance(mapOptions);
+                fragTx.Add(Resource.Id.map, _mapFragment, "map");
+                fragTx.Commit();
+            }
+            _mapFragment.GetMapAsync(this);
         }
 
         public static FragmentHome NewInstance()
@@ -33,8 +51,39 @@ namespace ProjectoFinal.Resources.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
+           // View v = inflater.Inflate(Resource.Layout.Fragemethome, container, false);
+
+            /*MapFragment mapFragment = (MapFragment)ChildFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.OnCreate(savedInstanceState);
+            mapFragment.GetMapAsync(this);*/
             return inflater.Inflate(Resource.Layout.Fragemethome, container, false);
 
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+
+
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            _map = googleMap;
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.SetPosition(new LatLng(16.03, 108));
+            markerOptions.SetTitle("test");
+            googleMap.AddMarker(markerOptions);
+            //zoom
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+            _map.setMyLocationEnabled(true);
+            if (_map != null)
+            {
+                _map.MapType = GoogleMap.MapTypeNormal;
+            }
         }
     }
 }
