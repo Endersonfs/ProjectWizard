c12cf01f9fcbb35c97240a756b3a79aa55dcf154
Paul12pp <30394467+Paul12pp@users.noreply.github.com>
2018-07-01 17:56:20 -0300

    Arreglo de fallos.

--- ProjectoFinal/ProjectoFinal/Resources/Fragments/FragmentHome.cs
+++ ProjectoFinal/ProjectoFinal/Resources/Fragments/FragmentHome.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
@@ -13,14 +15,30 @@ using Android.Widget;
 
 namespace ProjectoFinal.Resources.Fragments
 {
-    public class FragmentHome : Fragment
+    public class FragmentHome : Fragment, IOnMapReadyCallback
     {
+        private GoogleMap _map;
+        private MapFragment _mapFragment;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
-           
+            _mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+            if (_mapFragment == null)
+                           {
+               GoogleMapOptions mapOptions = new GoogleMapOptions()
+                    .InvokeMapType(GoogleMap.MapTypeSatellite)
+                    .InvokeZoomControlsEnabled(false)
+                    .InvokeCompassEnabled(true);
+                
+                FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+                _mapFragment = MapFragment.NewInstance(mapOptions);
+                fragTx.Add(Resource.Id.map, _mapFragment, "map");
+                fragTx.Commit();
+            }
+            _mapFragment.GetMapAsync(this);
+
         }
 
         public static FragmentHome NewInstance()
@@ -33,6 +51,7 @@ namespace ProjectoFinal.Resources.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
+
             return inflater.Inflate(Resource.Layout.Fragemethome, container, false);
 
         }
@@ -51,12 +70,11 @@ namespace ProjectoFinal.Resources.Fragments
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.SetPosition(new LatLng(16.03, 108));
             markerOptions.SetTitle("test");
-            googleMap.AddMarker(markerOptions);
+            _map.AddMarker(markerOptions);
             //zoom
-            googleMap.UiSettings.ZoomControlsEnabled = true;
-            googleMap.UiSettings.CompassEnabled = true;
-            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
-            _map.setMyLocationEnabled(true);
+            _map.UiSettings.ZoomControlsEnabled = true;
+            _map.UiSettings.CompassEnabled = true;
+            _map.MoveCamera(CameraUpdateFactory.ZoomIn());
             if (_map != null)
             {
                 _map.MapType = GoogleMap.MapTypeNormal;
